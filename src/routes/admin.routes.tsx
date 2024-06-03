cb33bdfc15540bb822cda9a6d8eaf703d563fa3f
Shakil53 <shakilahmed.dcc@gmail.com>
2024-06-03 23:08:33 +0600

    admin Sidebar layout done with programatic way

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,17 +3,24 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
 
 type TRoute = {
     path: string,
     element: ReactNode,
 }
 
+type TSidebarItem = {
+    key: string,
+    label: ReactNode,
+    children?: TSidebarItem[] 
+}
 
-export const admintPaths = [
+
+export const adminPaths = [
     {
         name: 'Dashboard',
-        path: '/admin/dashboard',
+        path: 'dashboard',
         element: <AdminDashboard></AdminDashboard>
     },
     {
@@ -34,14 +41,46 @@ export const admintPaths = [
                 path: 'create-student',
                 element: <CreateStudent></CreateStudent>
             },
+            {
+                name: 'Create Member',
+                path: 'create-Member',
+                element: <CreateStudent></CreateStudent>
+            },
         ]
     },
     
 ]
+// mainLayout programatic way
+export const adminSidebarItems = adminPaths.reduce(
+      (acc: TSidebarItem[], item) => {
+        if (item.path && item.name) {
+          acc.push({
+            key: item.name,
+            label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+          });
+        }
+    
+        if (item.children) {
+          acc.push({
+            key: item.name,
+            label: item.name,
+            children: item.children.map((child) => ({
+              key: child.name,
+              label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+            })),
+          });
+        }
+    
+        return acc;
+      },
+      []
+    );
 
-// programatic way
 
-export const adminRoutes = admintPaths.reduce((acc: TRoute[], item) => {
+
+// programatic way - routes
+
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
     if (item.path && item.element) {
         acc.push({
             path: item.path,
--- test.js
+++ test.js
@@ -38,24 +38,50 @@ const admintPaths2 = [
     
 ]
 
-
-
+// main layout- programatic way
 const newArray = admintPaths2.reduce((acc, item) => {
     if (item.path && item.element) {
         acc.push({
-            path: item.path,
-            element: item.element
+            key: item.name,
+            label: "NAVLINK"
        })
     }
     if (item.children) {
-        item.children.forEach((child) => {
-            acc.push({
-                path: child.path,
-                element: child.element
-            })
-        })
+        acc.push({
+            key: item.name,
+            label: item.name,
+            children: item.children.map(child => (
+                {
+                    key: child.name,
+                    label: "NAVLINK"
+                }
+            ))
+       })
     }
     return acc;
 }, [])
 
 console.log(newArray);
+
+
+
+
+// const newArray = admintPaths2.reduce((acc, item) => {
+//     if (item.path && item.element) {
+//         acc.push({
+//             path: item.path,
+//             element: item.element
+//        })
+//     }
+//     if (item.children) {
+//         item.children.forEach((child) => {
+//             acc.push({
+//                 path: child.path,
+//                 element: child.element
+//             })
+//         })
+//     }
+//     return acc;
+// }, [])
+
+// console.log(newArray);
